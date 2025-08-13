727d92a630c966897b4987faa4e068914519452e
Matt Pocock <mattpocockvoice@gmail.com>
2025-08-13 11:03:19 +0100

    Initial

---
+++ src/Cli.ts
@@ -0,0 +1,26 @@
+import { Args, Command, Options } from "@effect/cli"
+import { Console } from "effect"
+
+const prefix = Options.boolean("prefix").pipe(
+  Options.withDescription("Apply a prefix to the message")
+)
+
+const log = Command.make(
+  "log",
+  { message: Args.text(), prefix },
+  ({ message, prefix }) =>
+    Console.log(prefix ? `[${message}]` : message)
+)
+
+const internal = Command.make("internal").pipe(
+  Command.withSubcommands([log])
+)
+
+const command = Command.make("ai-hero").pipe(
+  Command.withSubcommands([internal])
+)
+
+export const run = Command.run(command, {
+  name: "AI Hero",
+  version: "0.0.0"
+})
