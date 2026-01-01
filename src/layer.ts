c98c49388a783c8ae0199981574b59c1bddb6a74
Matt Pocock <mattpocockvoice@gmail.com>
2026-01-01 17:34:00 +0000

    Added layer

--- src/layer.ts
+++ src/layer.ts
@@ -2,9 +2,11 @@ import { NodeContext } from "@effect/platform-node";
 import { Layer } from "effect";
 import { GitService } from "./git-service.js";
 import { LessonParserService } from "./lesson-parser-service.js";
+import { PromptService } from "./prompt-service.js";
 
 export const layerLive = Layer.mergeAll(
   NodeContext.layer,
   GitService.Default,
-  LessonParserService.Default
+  LessonParserService.Default,
+  PromptService.Default
 );
