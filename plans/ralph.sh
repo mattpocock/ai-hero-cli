04308c1fa4a6667f6b27ba46c588eb548c38f166
Matt Pocock <mattpocockvoice@gmail.com>
2026-01-02 15:12:26 +0000

    Ralph

--- plans/ralph.sh
+++ plans/ralph.sh
@@ -6,12 +6,12 @@ if [ -z "$1" ]; then
 fi
 
 for ((i=1; i<=$1; i++)); do
-  result=$(claude --permission-mode acceptEdits -p "@plans/prd.json @claude-progress.txt \
+  result=$(claude --permission-mode acceptEdits -p "@plans/prd.json @progress.txt \
 1. Find the highest-priority feature to work on and work only on that feature. \
 This should be the one YOU decide has the highest priority - not necessarily the first in the list. \
 2. Check that the types check via pnpm typecheck and that the tests pass via pnpm test. \
 3. Update the PRD with the work that was done. \
-4. Append your progress to the claude-progress.txt file. \
+4. Append your progress to the progress.txt file. \
 Use this to leave a note for the next person working in the codebase. \
 5. Make a git commit of that feature. \
 ONLY WORK ON A SINGLE FEATURE. \
